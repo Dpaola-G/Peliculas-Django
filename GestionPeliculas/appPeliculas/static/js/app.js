076fb38ee9f36c1db53c89aa6b4e633643b8c998
Dpaola-G <Dpaolag17@gmail.com>
2024-05-17 12:16:04 -0500

    mi primer commit

---
+++ GestionPeliculas/appPeliculas/static/js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Función que verifica si el usuario
+ * quiere eliminar una pelicula de acuerdo con
+ * su id.
+ * @param {*} id 
+ */
+function eliminarPelicula(id) {
+    Swal.fire({
+        title: "Desea eliminar la pelicula?",
+        showDenyButton: true,
+        confirmButtonText: "SI",
+        denyButtonText: "NO"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            location.href = "/eliminarPelicula/" + id
+        }
+    });
+}
+
